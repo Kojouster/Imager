68cd7bc48fb2c3657a97f5171048331534d19628
Kojouster <a2kojouster@gmail.com>
2022-09-22 17:41:57 +0300

    FinalCommit
    AR Game

---
+++ Assets/Scripts/ClickedButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickedButton : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    [SerializeField] private Image _img;
+    [SerializeField] private Sprite _default,_pressed;
+    [SerializeField] private AudioClip _compressedClip, _uncompressedClip;
+    [SerializeField] private AudioSource _source;
+
+
+   [SerializeField] public Animator tanim;
+   [SerializeField] string sceneName;
+
+    // these are effectively the same thing as onMouseDown and onMouseUp
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // When user taps the button it changes its state to pressed
+        _img.sprite = _pressed;
+        _source.PlayOneShot(_compressedClip);
+      
+       StartCoroutine(LoadScene());
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // When user taps the button it changes its state to unpressed
+        _img.sprite = _default;
+        _source.PlayOneShot(_uncompressedClip);
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1.5f);
+        tanim.SetTrigger("end");
+        yield return new WaitForSeconds(1.7f);
+         SceneManager.LoadScene(sceneName);
+    }
+
+
+}
---
+++ Assets/Scripts/ImageTracker.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageTracker : MonoBehaviour
+{
+    // creating a an array of prefabs that will be awailable at run time
+    [SerializeField]
+    private GameObject[] placeblePrefabs;
+
+    // this will call spawnned prefabs
+    private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
+
+    // creating AR tracker
+    private ARTrackedImageManager trackedImageManager;
+
+    private void Awake()
+    {
+        //getting and storing reference to the tracked image manager of type:
+        trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+        // looping through prefab gameobjects
+        foreach (GameObject prefab in placeblePrefabs)
+        {
+            //Instantiating the current loop prefab and setting the appropiarate scale and rotation
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
+            //making sure that the name is correct for serching it later
+            newPrefab.name = prefab.name;
+            // adding to spawned prefab dictionary
+            spawnedPrefabs.Add(prefab.name, newPrefab);
+
+        }
+    }
+    // Bind and UnBind images in the trackedImageManager
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageSwaped;
+    }
+
+    private void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= ImageSwaped;
+    }
+
+    //functionality based on which image is being tracked, removed or updated
+    private void ImageSwaped(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            // each time the image is added another function gets called
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            // each time the image is added another function gets called
+            UpdateImage(trackedImage);
+        }
+
+        foreach (ARTrackedImage trackedImage in eventArgs.removed)
+        {
+            // each time the image is added another function gets called
+            spawnedPrefabs[trackedImage.name].SetActive(false);
+        }
+
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        // temporarly storing the name of desired picture
+        string name = trackedImage.referenceImage.name;
+        Vector3 position = trackedImage.transform.position;
+
+        // getting prefab by the name
+        GameObject prefab = spawnedPrefabs[name];
+        // to see the prefab to the linked image
+        prefab.transform.position = position;
+        prefab.SetActive(true);
+
+        foreach (GameObject go in spawnedPrefabs.Values)
+        {
+            // When looking at the new Image the current one will be hidden
+            if (go.name != name)
+            {
+                go.SetActive(false);
+            }
+        }
+
+    }
+}
